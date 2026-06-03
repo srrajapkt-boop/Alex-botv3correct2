716f6a180de439de1ae713c516159328d51595c5
srrajapkt-boop <srraja.pkt@gmail.com>
2026-06-03 20:36:44 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,24 @@
 #include <micro_ros_arduino.h>
 #include <stdio.h>
 #include <rcl/rcl.h>
+#include <rcl/error_handling.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 #include <std_msgs/msg/string.h>
 #include <ESP32Servo.h>
 #include <Adafruit_SSD1306.h>
 
-Servo sL, sR;
-Adafruit_SSD1306 display(128, 64, &Wire, -1);
-
+// --- ADD THESE GLOBAL DECLARATIONS ---
 rcl_subscription_t subscriber;
 std_msgs__msg__String msg;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+rclc_executor_t executor;
+// --------------------------------------
+
+Servo sL, sR;
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
 void subscription_callback(const void *msgin) {
   const std_msgs__msg__String * msg = (const std_msgs__msg__String *)msgin;
@@ -22,16 +29,25 @@ void subscription_callback(const void *msgin) {
 }
 
 void setup() {
-  // IMPORTANT: Set this to the IP assigned to your Termux/Linux environment
-  set_microros_wifi_transports("YOUR_HOTSPOT_NAME", "YOUR_PASSWORD", "192.168.4.2", 8888);
+  // Use a proper char* cast to fix the warning
+  char ssid[] = "YOUR_HOTSPOT_NAME";
+  char pass[] = "YOUR_PASSWORD";
+  char ip[] = "192.168.4.2";
+  
+  set_microros_wifi_transports(ssid, pass, ip, 8888);
   
   sL.attach(13); sR.attach(12);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 
-  rcl_allocator_t allocator = rcl_get_default_allocator();
+  allocator = rcl_get_default_allocator();
   rclc_support_init(&support, 0, NULL, &allocator);
   rclc_node_init_default(&node, "alex_bot", "", &support);
+  
   rclc_subscription_init_default(&subscriber, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String), "alex_cmd");
+  
+  executor = rclc_executor_get_zero_initialized_executor();
+  rclc_executor_init(&executor, &support.context, 1, &allocator);
+  rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA);
 }
 
 void loop() {
